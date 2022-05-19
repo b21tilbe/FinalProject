96e674e786ceace62e79a854bef85f8682862ac8
hey <b21tilbe@student.his.se>
2022-05-19 15:12:37 +0200

    Lagt till itemView och textView i ViewHolder.java filen

--- app/src/main/java/com/example/finalproject/ViewHolder.java
+++ app/src/main/java/com/example/finalproject/ViewHolder.java
@@ -1,4 +1,22 @@
 package com.example.finalproject;
 
-public class ViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView name;
+    public TextView category;
+    public TextView size;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name = itemView.findViewById(R.id.cupcakeName);
+        category = itemView.findViewById(R.id.category);
+        size = itemView.findViewById(R.id.size);
+    }
 }
