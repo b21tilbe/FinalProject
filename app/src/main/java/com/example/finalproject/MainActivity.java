e4de482560e932e359c5495ca64e72bd5fc5a553
hey <b21tilbe@student.his.se>
2022-05-19 15:05:49 +0200

    Fixat lite i koden. Bland annat fixat textView:s i "recyclerview_item.xml" filen.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21tilbe";
+    //private final String JSON_FILE = "mountains.json";
+
+    private List<Cupcake> cupcakeList;
+    private CupcakeAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
