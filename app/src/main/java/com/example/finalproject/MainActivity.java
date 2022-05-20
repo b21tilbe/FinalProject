3f2a490f4fde2d5202b9113d6da213973e607807
b21tilbe <tilda.benerdal@hotmail.se>
2022-05-20 16:45:52 +0200

    Skapat knappar i båda activity filerna. Sedan kopplat dem med varandra

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -27,12 +30,21 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.start_second_activity);
         adapter = new CupcakeAdapter();
         cupcakeList = new ArrayList<Cupcake>();
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_second.class);
+                startActivity(intent);
+            }
+        });
 
         new JsonTask(this).execute(JSON_URL);
     }
--- app/src/main/java/com/example/finalproject/activity_second.java
+++ app/src/main/java/com/example/finalproject/activity_second.java
@@ -2,13 +2,31 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_second extends AppCompatActivity {
+
+    Button close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        close = findViewById(R.id.close_second_activity);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity_second.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
